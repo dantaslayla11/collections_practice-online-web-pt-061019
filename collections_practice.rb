3e8cb5ff46f904f7fabfba6aa2a2a4381fe37e40
layla dantas <laylatdantas@gmail.com>
2019-06-12 01:56:15 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,59 @@
+require "pry"
 
+
+def sort_array_asc(array)
+  array.sort
+  end
+
+def sort_array_desc(array)
+  
+  array.sort {|x,y| -(x <=> y)}
+  
+end
+
+def sort_array_char_count(array)
+  
+  array = array.sort_by {|x| x.length}
+  
+end
+
+def swap_elements(array)
+
+end
+
+def reverse_array(array)
+  array.reverse
+  
+end
+
+
+def kesha_maker(array)
+  
+  array.each do |item|
+   
+   #binding.pry
+    
+      
+    end 
+    
+  
+ 
+end
+
+
+
+def find_a(array)
+  array.find { |x| x.start_with?("a") }
+ 
+   end
+ 
+
+
+
+
+
+
+
+
+
+  
